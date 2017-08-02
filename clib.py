40de6b545583f9f15ddd55cecd499d2196623ec5
manasses23 <manasses.silva23@gmail.com>
2017-08-02 18:31:45 +0000

    Update clib.py

--- clib.py
+++ clib.py
@@ -1,23 +1,22 @@
 import socket
 from datetime import datetime
 HOST = "localhost"
-PORT = 2000
+PORT = 4000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST,PORT))
 
 while 1:
-	datahora = conn.recv(10240)
-	data = repr(datahora)[2:-1]
+	
 	message = input("Your Message: ")
 	sock.send(bytes(message,'UTF-8'))
 	if (message == '#QUEHORAS'):
-		print (data)
-	elif (message == '#CLOSE_CONNECTION'):
+		print ("As horas são:")
+	elif (message == '#QUIT'):
 		print ("CONNECTION_WILL_BE_CLOSED")
 		break
-	elif (message == '#CLOSE_CONNECTION'):
-		print ("CONNECTION_WILL_BE_CLOSED")
-		break
+	elif (message == '#REGISTRAR'):
+		print ("Qual o nome?")
+		
 
 
 sock.close()
