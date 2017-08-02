63a14deb26fa5c192e667790006ce16f1e50f715
manasses23 <manasses.silva23@gmail.com>
2017-08-02 21:07:44 +0000

    Update servb.py

--- servb.py
+++ servb.py
@@ -2,7 +2,7 @@ import socket
 from datetime import datetime
 import json
 HOST = "" 
-PORT = 4000
+PORT = 3000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
@@ -18,23 +18,35 @@ while 1:
 	logg.close
 	logg=open('log.json')
 	log=json.load(logg)
-
-	raw_data = conn.recv(10240)
+	registro=[]
+	arq = open('/tmp/lista.txt', 'w' )
+	registro.append(log)
+	arq.writelines(registro ) 
+	arq = open('/tmp/lista.txt', 'r')
+	registro = arq.readlines()
+	arq.close
+	
+	raw_data = conn.recv(1024)
 	data = repr(raw_data)[2:-1]
 	print(data)
 	if (data == '#QUEHORAS'):
-		print ("A hora é:",str(datahora.hour)+':'+ str(datahora.minute) +':'+ str(datahora.second))
+		hora = "HORA: hora é:",str (datahora.hour)+':'+ str(datahora.minute) +':'+ str(datahora.second)
+		#byte_msg = bytes(hora('utf-8')
+		#conn.send(byte_msg)
+		print (hora)	
 
 	elif (data =='#QUIT'):
 		print ("CONNECTION_WILL_BE_CLOSED")
 		break
 	elif (data =='#REGISTRAR'):
 		print ("Digite o nome")
-		 
-	elif (data =='#QUEDATA'):
-		print ("A data é:",str(datahora.day) +'/'+ str(datahora.month) +'/'+ str(datahora.year))
-	         
-		 
-	elif (data !="#QUEHORAS" or data !="#CLOSE_CONNECTION"or data!='#QUEDATA'or data!='#REGISTRAR'or data!='#LISTA'):
+	elif (data =='#QUEDIA'):
+		print ("DATA: ",str(datahora.day) +'/'+ str(datahora.month) +'/'+ str(datahora.year))
+	elif (data =='#QUIT'):
+		print ("CONNECTION_WILL_BE_CLOSED")
+		break	
+	elif (data =='#LISTA'):
+		print (registro)
+	elif (data !="#QUEHORAS" or data !="#CLOSE_CONNECTION"or data!='#QUEDIA'or data!='#REGISTRAR'or data!='#LISTA'):
 		print ("FAULT 512")
 conn.close()
